c0c955e0023f69251f37a5881d7c17c69c6ab6ed
Achraf EL KADIRI <62653000+AchrafELKADIRI99@users.noreply.github.com>
2021-06-13 00:59:57 +0100

    change cursor to pointer with role=button to form

--- views/Products.php
+++ views/Products.php
@@ -23,12 +23,14 @@ $products = $data->getAllProducts();
                 ?>
             
              <div class="col-4 ">
+             <form  role="button" id="form" onclick="submitForm(<?php echo $productId; ?>)" name="submit" method="post" action="<?php echo BASE_URL;?>singleProduct">
                     <img src="<?php echo $product['prod_image']; ?>"> 
-                         <h4 onclick="submitForm(<?php echo $productId; ?>)">
+                   
+
+                         <h4 >
                          <?php echo $product['prod_title']; ?>
                          </h4>
                         <p><?php echo $product['prod_price']; ?></p>
-                        <form id="form" name="submit" method="post" action="<?php echo BASE_URL;?>singleProduct">
                         <input type="hidden" name="productId" id="prodId"  class="btn btn-warning" ></input>
                         </form>
                     </div>
